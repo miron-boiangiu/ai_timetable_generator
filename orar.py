86f7f9091dd2187eaceabc6ad05b47c528d99258
Victor-Miron Boiangiu <miron.boiangiu@gmail.com>
2024-04-25 16:14:10 +0300

    feat: change data structure
    Signed-off-by: Victor-Miron Boiangiu <miron.boiangiu@gmail.com>

--- orar.py
+++ orar.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import os
 import copy
 import random
@@ -9,87 +10,15 @@ from check_constraints import *
 from utils import *
 
 # State macros
-INTERVAL_POS = 0
-PROF_NAME_POS = 1
-DAY_POS = 2
-CLASSROOM_POS = 3
-SUBJECT_POS = 4
-
-# The format of the state is a tuple of tuples, consisting of what entries there are so far:
-# (((interval_start, interval_end), "Prof name", "Day", "Classroom", "Subject"), ...)
-
+PROF_INTERVALS_NO = "_PROF_INTERVALS_NO"
+STUDENTS_TO_ASSIGN_PER_SUBJECT = "_STUDENTS_LEFT_TO_ASSIGN"
+ENTRIES_NO = "_ENTRIES_NO"
 
 def all_covered(state, timetable_specs):
     # Considering the state is valid, are all subjects covered?
 
-    subjects = copy.deepcopy(timetable_specs[MATERII])
-    classrooms = timetable_specs[SALI]
-
-    for interval, prof_name, day, classroom, subject in state:
-        subjects[subject] -= classrooms[classroom][CAPACITATE]
-
-    for subject, capacitate in subjects.items():
-        if capacitate > 0:
-            return False
-
-    return True
-
-
-def get_uncovered(state, timetable_specs):
-    # Considering the state is valid, are all subjects covered?
-
-    subjects = copy.deepcopy(timetable_specs[MATERII])
-    classrooms = timetable_specs[SALI]
-
-    for interval, prof_name, day, classroom, subject in state:
-        subjects[subject] -= classrooms[classroom][CAPACITATE]
-
-    unfinished_subjects = []
-
-    for subject, capacitate in subjects.items():
-        if capacitate > 0:
-            unfinished_subjects.append((subject, capacitate))
-
-    unfinished_subjects.sort(key=lambda a: a[1])
-    return unfinished_subjects
-
-
-def is_valid_state(state, timetable_specs):
-    # Are all hard constraints respected?
-
-    number_of_entries = len(state)
-
-    prof_interval_count = {}
-    for prof in timetable_specs[PROFESORI].keys():
-        prof_interval_count[prof] = 7
-
-    for i in range(number_of_entries):
-        
-        entry_i = state[i]
-
-        # Can the teacher even teach this?
-        if entry_i[SUBJECT_POS] not in timetable_specs[PROFESORI][entry_i[PROF_NAME_POS]][MATERII]:
-            return False
-
-        # Is the classroom fit for this subject?
-        if entry_i[SUBJECT_POS] not in timetable_specs[SALI][entry_i[CLASSROOM_POS]][MATERII]:
-            return False
-        
-        prof_interval_count[entry_i[PROF_NAME_POS]] -= 1
-
-        for j in range(i + 1, number_of_entries):
-            entry_j = state[j]
-
-            #  Is the same professor teaching in the same interval?
-            if entry_i[PROF_NAME_POS] == entry_j[PROF_NAME_POS] and entry_i[DAY_POS] == entry_j[DAY_POS] and entry_i[INTERVAL_POS] == entry_j[INTERVAL_POS]:
-                return False
-
-            #  Is the same classroom occupied in the same interval?
-            if entry_i[CLASSROOM_POS] == entry_j[CLASSROOM_POS] and entry_i[DAY_POS] == entry_j[DAY_POS] and entry_i[INTERVAL_POS] == entry_j[INTERVAL_POS]:
-                return False
-
-    for prof_name, interval_count in prof_interval_count.items():
-        if interval_count < 0:
+    for materie, studenti_ramasi in state[STUDENTS_TO_ASSIGN_PER_SUBJECT].items():
+        if studenti_ramasi > 0:
             return False
 
     return True
@@ -101,53 +30,106 @@ def count_weak_constraints(state, timetable_specs):
     return count
 
 
+def pick_subject(state, timetable_specs):
+    least_students_remaining = ("", float('inf'))
+
+    for subject, students in state[STUDENTS_TO_ASSIGN_PER_SUBJECT].items():
+        if students <= 0:
+            continue
+
+        if students < least_students_remaining[1]:
+            least_students_remaining = (subject, students)
+
+    return least_students_remaining[0]
+
+
 def state_neighbours(state, timetable_specs):
 
-    unfinished_subject = get_uncovered(state, timetable_specs)[0][0]
+    unfinished_subject = pick_subject(state, timetable_specs)
     valid_profs = [prof for prof in timetable_specs[PROFESORI].keys() if unfinished_subject in timetable_specs[PROFESORI][prof][MATERII]]
+    valid_profs = list(filter(lambda prof: state[PROF_INTERVALS_NO][prof] > 0, valid_profs))
     valid_classrooms = [classroom for classroom in timetable_specs[SALI].keys() if unfinished_subject in timetable_specs[SALI][classroom][MATERII]]
 
-    state = list(state)
-    new_entries = [(interval, name, day, classroom, unfinished_subject) for interval in timetable_specs[INTERVALE] for name in valid_profs for day in timetable_specs[ZILE] for classroom in valid_classrooms]
-    random.shuffle(new_entries)
-    new_states = [state + [entry] for entry in new_entries]
-    neighbours = list(filter(lambda a: is_valid_state(a, timetable_specs), new_states))
-    for entry in neighbours:
-        entry.sort()
+    new_states = []
 
-    # print("Computed", len(neighbours), "neighbours")
-    return map(lambda a: tuple(a), neighbours)
+    for day in timetable_specs[ZILE]:
+        for interval in state[day]:
+
+            profs_in_interval = [tup[0] for sala, tup in state[day][interval].items()]
+            available_profs = [prof for prof in valid_profs if prof not in profs_in_interval]
+
+            for classroom in valid_classrooms:
+
+                if classroom in state[day][interval]:
+                    continue
+
+                for prof in available_profs:
+                    new_state = copy.deepcopy(state)
+                    new_state[ENTRIES_NO] += 1
+                    new_state[day][interval][classroom] = (prof, unfinished_subject)
+                    new_state[STUDENTS_TO_ASSIGN_PER_SUBJECT][unfinished_subject] -= timetable_specs[SALI][classroom][CAPACITATE]
+                    new_state[PROF_INTERVALS_NO][prof] -= 1
+                    new_states.append(new_state)
+
+    print("Computed", len(new_states), "neighbours")
+    random.shuffle(new_states)
+    return new_states
 
 
-def f(state, timetable_specs, max_classroom_size, min_classroom_size):
-    unfinished_subjects = get_uncovered(state, timetable_specs)
+def h(state, timetable_specs, min_classroom_size):
+    unfinished_subjects = [tup for tup in state[STUDENTS_TO_ASSIGN_PER_SUBJECT].items() if tup[1] > 0]
 
     if len(unfinished_subjects) == 0:
-        return len(state)
+        return 0
 
     leftover_students = sum(map(lambda a: a[1], unfinished_subjects))
-    return len(state) + (leftover_students / min_classroom_size)
+    return leftover_students / min_classroom_size
+
+
+def g(state):
+    return state[ENTRIES_NO]
+
+
+def generate_initial_timetable(timetable_specs):
+    timetable = {}
+
+    for day in timetable_specs[ZILE]:
+        timetable[day] = {}
+
+        for interval in timetable_specs[INTERVALE]:
+            interval = literal_eval(interval)
+            timetable[day][interval] = {}
+
+    timetable[ENTRIES_NO] = 0
+
+    timetable[STUDENTS_TO_ASSIGN_PER_SUBJECT] = {}
+    for subject, students_needed in timetable_specs[MATERII].items():
+        timetable[STUDENTS_TO_ASSIGN_PER_SUBJECT][subject] = students_needed
+
+    timetable[PROF_INTERVALS_NO] = {}
+    for prof in timetable_specs[PROFESORI]:
+        timetable[PROF_INTERVALS_NO][prof] = 7
+
+    return timetable
 
 
 def astar(timetable_specs, max_iters = -1):
 
-    max_classroom_size = 0
     min_classroom_size = float('inf')
     for classroom in timetable_specs[SALI]:
-        if timetable_specs[SALI][classroom][CAPACITATE] > max_classroom_size:
-            max_classroom_size = timetable_specs[SALI][classroom][CAPACITATE]
-
         if timetable_specs[SALI][classroom][CAPACITATE] < min_classroom_size:
             min_classroom_size = timetable_specs[SALI][classroom][CAPACITATE]
 
-    start = ()
+    start = generate_initial_timetable(timetable_specs)
     frontier = []
-    heappush(frontier, (0, start))
+    heappush(frontier, (0, 0, start))
 
-    discovered = {start: 0}
+    discovered = {str(start): 0}
+
+    heap_entries_count_priority = 0
 
     while frontier:
-        _, node = heappop(frontier)
+        _, _, node = heappop(frontier)
 
         if max_iters > 0:
             max_iters -= 1
@@ -157,47 +139,50 @@ def astar(timetable_specs, max_iters = -1):
         if all_covered(node, timetable_specs):
             return node
 
-        # print("Looking at a node with", len(node), "entries")
+        print("Looking at a node with", node[ENTRIES_NO], "entries")
 
         for n in state_neighbours(node, timetable_specs):
-            if n not in discovered:  #TODO: Reimplement this
-                heappush(frontier, (f(n, timetable_specs, max_classroom_size, min_classroom_size), n ) )
-                discovered[n] = discovered[node] + 1
+            state_key = str(n)
 
-        # print("Heap now has", len(frontier), "elements")
-        # print(frontier[0][0])
+            if state_key not in discovered:
+                heap_entries_count_priority -= 1
+                heappush(frontier, (g(n) + h(n, timetable_specs, min_classroom_size), heap_entries_count_priority, n) )
+                discovered[state_key] = g(n)
+
+        print("Heap now has", len(frontier), "elements")
         # frontier = frontier[:500]
 
     return None
 
 
-def convert_timetable_format(timetable, timetable_specs):
-    new_timetable = {}
-    interval_dict = {}
+def prepare_output(result, timetable_specs):
+    del result[ENTRIES_NO]
+    del result[PROF_INTERVALS_NO]
+    del result[STUDENTS_TO_ASSIGN_PER_SUBJECT]
 
-    for interval in timetable_specs[INTERVALE]:
-        interval_dict[literal_eval(interval)] = {}
+    for day in timetable_specs[ZILE]:
+        for interval in timetable_specs[INTERVALE]:
+            
+            interval = literal_eval(interval)
+            for sala in timetable_specs[SALI].keys():
 
-        for sala in timetable_specs[SALI].keys():
-            interval_dict[literal_eval(interval)][sala] = {}
+                if sala not in result[day][interval]:
+                    result[day][interval][sala] = None
 
-    for zi in timetable_specs[ZILE]:
-        new_timetable[zi] = copy.deepcopy(interval_dict)
+    return result
 
-    for entry in timetable:
-        new_timetable[entry[DAY_POS]][literal_eval(entry[INTERVAL_POS])][entry[CLASSROOM_POS]] = (entry[PROF_NAME_POS], entry[SUBJECT_POS])
-
-    return new_timetable
 
 def run_a_star(input_file_name, timetable_specs, input_path):
     print("Running a*.")
-    result = astar(timetable_specs, 1000)
+    result = astar(timetable_specs, 200)
+    # print(result)
+    print(result[STUDENTS_TO_ASSIGN_PER_SUBJECT])
 
-    proper_format = convert_timetable_format(result, timetable_specs)
-    print(pretty_print_timetable(proper_format, input_path))
+    result = prepare_output(result, timetable_specs)
+    print(pretty_print_timetable(result, input_path))
 
     # print("Constrangeri soft:", check_optional_constraints(proper_format, timetable_specs))
-    print("Constrangeri hard:", check_mandatory_constraints(proper_format, timetable_specs))
+    # print("Constrangeri hard:", check_mandatory_constraints(result, timetable_specs))
 
 if __name__ == "__main__":
 
